ec6f5f63202b8ef9898feac536f1d475d5cac196
baichen528 <jsjchenchao@126.com>
2020-04-20 13:59:20 +0800

    修改Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
 public class User_Spring_Test {
 	
 	public static void main(String[] args) {
-		System.out.println("第一次使用github创建的java文件");
+		System.out.println("第一次使用github创建的java文件"); 1111
 	}
+	oublic int add(int a,int b){
+		return a+b;
+	}
+	
+	
 }
 	
